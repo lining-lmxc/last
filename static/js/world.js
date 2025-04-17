2b609dae7ce8f135a903aa1aa506f390502ef274
Lining <lmxc162149526@petalmail.com>
2025-04-17 15:46:11 +0800

    2025/4/17 第一版

---
+++ static/js/world.js
@@ -0,0 +1,114 @@
+// 注册世界地图
+echarts.registerMap('world', {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "China"
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [[[73.68, 35.42], [73.68, 35.42], [73.68, 35.42], [73.68, 35.42]]]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Japan"
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [[[139.69, 35.69], [139.69, 35.69], [139.69, 35.69], [139.69, 35.69]]]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Korea"
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [[[126.98, 37.57], [126.98, 37.57], [126.98, 37.57], [126.98, 37.57]]]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "India"
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [[[77.21, 28.61], [77.21, 28.61], [77.21, 28.61], [77.21, 28.61]]]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "United Kingdom"
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [[[-0.13, 51.51], [-0.13, 51.51], [-0.13, 51.51], [-0.13, 51.51]]]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "France"
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [[[2.35, 48.86], [2.35, 48.86], [2.35, 48.86], [2.35, 48.86]]]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Germany"
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [[[13.40, 52.52], [13.40, 52.52], [13.40, 52.52], [13.40, 52.52]]]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Russia"
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [[[37.62, 55.75], [37.62, 55.75], [37.62, 55.75], [37.62, 55.75]]]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "United States"
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [[[-74.01, 40.71], [-74.01, 40.71], [-74.01, 40.71], [-74.01, 40.71]]]
+                ]
+            }
+        }
+    ]
+}); 
